a03544c55a0abe63c5b78325506b62cb845ff99e
sasinha <sasinha@ucsc.edu>
2016-10-15 13:01:48 -0700

    soln to letters_to_numbers

---
+++ letters_to_numbers.py
@@ -0,0 +1,12 @@
+# Write a program that assigns letters to an array of numbers
+
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+dictionary = dict(zip(letters, range(0, 26)))
+
+input_word = input("What is your word: ")
+
+output = [dictionary[x] for x in input_word]
+
+print(output)
